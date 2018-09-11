b96426a378db31c038a1050673943761aa52ccfe
Marisol Smith <marisolsmith@gmail.com>
2018-09-10 21:24:26 -0300

    color picker 2

--- app/assets/javascripts/knob.js
+++ app/assets/javascripts/knob.js
@@ -1,11 +1,18 @@
 $(function() {
-    $(".dial").knob({
-      'change': function() {
-        var color_rgb ='rgb(' + $('#r').val() + ',' + $('#g').val() + ',' + $('#b').val() + ')';
-        }
-
-    });
-
+  $(".dial").knob({
+    'change': function() {
+      var color_rgb='rgb(' + $('#r').val() + ',' + $('#g').val() + ',' + $('#b').val() + ')';
+      $('#color').css('background-color',color_rgb);
+      var color_hex = '#' + d_a_h($('#r').val()) + d_a_h($('#g').val()) + d_a_h($('#b').val());
+      $('#color').text(color_hex);
+    }
+  });
 });
 
-$('#color').css('background-color',color_rgb);
+function d_a_h(dec) {
+  var caracteres = "0123456789ABCDEF";
+  var low = dec % 16;
+  var high = (dec - low) / 16;
+  hex = "" + caracteres.charAt(high) + caracteres.charAt(low);
+  return hex;
+}
